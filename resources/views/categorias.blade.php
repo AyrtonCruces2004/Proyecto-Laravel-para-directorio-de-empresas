da097f97b186ed69399be9feb10a4226bdd663fa
Calderon Cruces Jose Ayrton <u21215967@utp.edu.pe>
2025-08-09 16:08:41 -0500

    09-08-2025
    Casi se me borra lo que hice XD

--- resources/views/categorias.blade.php
+++ resources/views/categorias.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Categorias</title>
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
     <style>
